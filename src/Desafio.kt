969a8fdf729d08a16c982a034909f8e6b89b9bc9
Willian Jean <willianjean@gmail.com>
2021-03-31 23:04:05 -0400

    Primeiro desafio programação kotling DIO

---
+++ src/Desafio.kt
@@ -0,0 +1,34 @@
+/*
+import java.util.Scanner
+
+fun main (){
+    val input = Scanner(System.`in`)
+    val a = input.nextInt()
+    val b = input.nextInt()
+
+    println("SOMA = "+ soma (  a +  b ))  //insira as variáveis corretamente
+}
+
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+
+    for (i in 1.rangeTo(readLine()!!.toInt()).step(2)) println(i) //complete o codigo com os valores corretos
+
+}
+
+
+fun main(args: Array<String>) {
+    //insira as variaveis corretamente
+    var r = 0
+    for (i in 1..readLine()!!.toInt()) {
+
+        println("$i ${  i*i   } ${  i*i*i  }")
+
+        r += 0
+    }
+}
+
+*/
+
