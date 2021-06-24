98352673141d10e499cbda410cce569342eed58d
willianjean <willianjean@gmail.com>
2021-06-24 11:41:47 -0400

    desafios para consulta de implementação futura
    minhas soluções

--- src/Desafio.kt
+++ src/Desafio.kt
@@ -30,5 +30,39 @@ fun main(args: Array<String>) {
     }
 }
 
+
+
+
+import java.util.Scanner
+
+fun main(args: Array<String>) {
+  var reader = Scanner(System.`in`)
+
+  var a = reader.nextDouble()
+  var b = reader.nextDouble()
+    
+  var weightA = 3.5
+  var weightB = 7.5
+  var totalWeight = weightA + weightB
+    
+  var m = (a * weightA + b * weightB) / totalWeight
+
+  var r = "%.5f".format(m)
+    
+  println("MEDIA = ${r}")
+}
+
+
+
+
+fun main(args: Array<String>) {
+
+  val a = readLine()!!.toInt()
+  val b = readLine()!!.toInt()
+
+  println("PROD = ${a*b}")   //insira as variaveis corretamente
+
+}
+
 */
 
